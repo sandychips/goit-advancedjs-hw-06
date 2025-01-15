a430771a19a78d2e60315093ae7fa1c44eeb9939
Chips <149303347+sandychips@users.noreply.github.com>
2025-01-15 18:00:53 +0200

    Typed files

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -1,17 +1,19 @@
 /*
-  Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
+  Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа.
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
-  return new Promise((resolve) => {
-    resolve(['Text', 50]);
-  });
+type PromiseType = (string | number)[]
+
+function getPromise(): Promise<PromiseType> {
+    return new Promise((resolve) => {
+        resolve(["Text", 50]);
+    });
 }
 
 getPromise()
-.then((data) => {
-  console.log(data);
-});
+    .then((data: PromiseType): void => {
+        console.log(data);
+    });
 
 export {};
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,20 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface IProps {
+    title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
-    console.log(this.props.title);
-  }
+class Component<T> {
+    constructor(public props: T) {
+
+    }
+}
+
+class Page extends Component<IProps> {
+    pageInfo() {
+        console.log(this.props.title);
+    }
 }
 
 export {};
